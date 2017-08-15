bbb4303ef0ca4c4b0ef193cb4e3f19a9121ce8e2
rob dark <robdark@robs-MacBook-Pro.local>
2017-08-15 15:19:19 -0500

    third commit

--- app.js
+++ app.js
@@ -4,24 +4,23 @@ const mustache = require("mustache-express")
 const data = require("./data")
 app.engine('mustache', mustache())
 app.set('view engine', 'mustache')
-app.use( express.static('public'))
+app.use(express.static('public'))
 
-app.get("/", function(request, response){
+app.get("/", function(request, response) {
   response.render('index', {
     robots: data.users
   })
 })
 
-app.listen(3000, function(){
-})
+app.listen(3000, function() {})
 
-app.get('/users/:id', function(request, response){
+app.get('/users/:id', function(request, response) {
 
   const robotId = parseInt(request.params.id)
   console.log(robotId);
   let person = false;
   for (var i = 0; i < data.users.length; i++) {
-    if (data.users[i].id === robotId){
+    if (data.users[i].id === robotId) {
       person = data.users[i]
     }
   }
